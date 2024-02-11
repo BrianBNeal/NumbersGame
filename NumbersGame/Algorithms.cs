f5a17b49d803ecc14a177fb4ab81ac57ca977a55
Brian Neal <brianbneal@gmail.com>
2024-02-11 14:02:45 -0600

    implement subtraction
    definitely still brute force

--- NumbersGame/Algorithms.cs
+++ NumbersGame/Algorithms.cs
@@ -2,9 +2,32 @@
 {
     internal static ArithmaticExpression? Solve(ProblemStatement problem)
     {
-        return problem.InputNumbers
-            .Where(x => x == problem.Target)
-            .Select(x => new ArithmaticExpression(x))
-            .FirstOrDefault();
+        Queue<ArithmaticExpression> combining = new(
+            problem.InputNumbers.Select(x => new ArithmaticExpression(x)));
+
+        List<ArithmaticExpression> known = new();
+
+        while (combining.TryDequeue(out ArithmaticExpression? current)) 
+        { 
+            if (current.Value == problem.Target) return current;
+
+            IEnumerable<ArithmaticExpression> combinableWith = known
+                .Where(x => !x.UsedNumbers.Intersect(current.UsedNumbers).Any());
+
+            foreach (var existing in combinableWith)
+            {
+                combining.Enqueue(
+                    current.CombineWith(existing, '+', current.Value + existing.Value));
+                
+                combining.Enqueue(
+                    current.CombineWith(existing, '-', current.Value - existing.Value));
+                combining.Enqueue(
+                    existing.CombineWith(current, '-', existing.Value - current.Value));
+            }
+
+            known.Add(current);
+        }
+
+        return null;
     }
 }
--- NumbersGame/ArithmaticExpression.cs
+++ NumbersGame/ArithmaticExpression.cs
@@ -1,14 +1,38 @@
-﻿internal class ArithmaticExpression
+﻿
+internal class ArithmaticExpression
 {
+    public int Value { get; }
+    public IEnumerable<int> UsedNumbers { get; }
+    public char Operator { get; }
+    public ArithmaticExpression LeftChild { get; }
+    public ArithmaticExpression RightChild { get; }
+
     public ArithmaticExpression(int value)
     {
         Value = value;
+        UsedNumbers = new[] { value };
+        Operator = '\0';
     }
 
-    public int Value { get; }
-
-    public override string ToString()
+    private ArithmaticExpression(int value, char @operator, ArithmaticExpression leftChild, ArithmaticExpression rightChild)
     {
-        return $@"{Value} = {Value}";
+        Value = value;
+        Operator = @operator;
+        UsedNumbers = leftChild.UsedNumbers.Union(rightChild.UsedNumbers);
+        LeftChild = leftChild;
+        RightChild = rightChild;
     }
+
+    public ArithmaticExpression CombineWith(ArithmaticExpression other, char @operator, int value) => 
+        new ArithmaticExpression(value, @operator, this, other);
+
+    public override string ToString() =>
+        $"{PlainToString(this)} = {Value}";
+
+    private string PlainToString(ArithmaticExpression expr) =>
+        expr.Operator == '\0' ? $"{expr.Value}"
+        : $"{expr.Parenthesize(expr.LeftChild)} {expr.Operator} {expr.Parenthesize(expr.RightChild)}";
+
+    private string Parenthesize(ArithmaticExpression child) =>
+        child.Operator == '\0' ? $"{child.Value}" : $"({PlainToString(child)})";
 }
